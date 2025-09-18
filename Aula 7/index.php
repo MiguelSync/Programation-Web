4d303878e6f0f4f41f844f753b3d88bd36dbed6a
Miguel Antonio Selhorst <143762650+MiguelSync@users.noreply.github.com>
2025-09-18 19:53:03 -0300

    create index.php

---
+++ Aula 7/index.php
@@ -0,0 +1,11 @@
+<!DOCTYPE html>
+<html lang="pt-BR">
+<head>
+  <meta charset="UTF-8">
+  <title>Meu primeiro PHP</title>
+</head>
+<body>
+  <h1>Bem-vindo!</h1>
+  <p>Agora são: <?php echo date("d/m/Y H:i:s"); ?></p>
+</body>
+</html>
